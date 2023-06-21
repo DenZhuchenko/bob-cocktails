27088c1393f9e936e0883dadb2a68f185fd2b8e4
DenZhuchenko <NavisNobilitae@gmail.com>
2023-06-21 21:17:51 +0300

    Some refactoring

---
+++ src/components/routes/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from '../layout/appLayout';
+import MainLayout from '../layout/mainLayout';
+import GoodsSelection from '../main/goodsSelection';
+import Cocktail from '../main/goodsSelection/Cocktail/Cocktail';
+import AuthLayout from '../auth/authLayout';
+import Registration from '../auth/registration';
+import Login from '../auth/login';
+import Basket from '../header/basket/basket';
+import PaymentLayout from '../payment/paymentLayout';
+import Payment from '../payment';
+import PageNotFound from '../pageNotFound';
+
+const RouterPage = () => {
+  return (
+    <Routes>
+      <Route path={'/'} element={<AppLayout />}>
+        <Route path={'/'} element={<MainLayout />}>
+          <Route path={':ingredientName'} element={<GoodsSelection />} />
+          <Route path={`cocktail/:id`} element={<Cocktail />} />
+        </Route>
+
+        <Route
+          path={`registration`}
+          element={
+            <AuthLayout>
+              <Registration />
+            </AuthLayout>
+          }
+        />
+        <Route path={'test'} element={<p>Test</p>} />
+
+        <Route
+          path={`login`}
+          element={
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
+          }
+        />
+
+        <Route path={'basket'} element={<Basket />} />
+        <Route
+          path={'payment'}
+          element={
+            <PaymentLayout>
+              <Payment />
+            </PaymentLayout>
+          }
+        />
+
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default RouterPage;
