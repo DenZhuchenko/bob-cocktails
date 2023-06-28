269623e214ac35492e174ba3b50aa7272acbbbc5
DenZhuchenko <NavisNobilitae@gmail.com>
2023-06-28 19:48:41 +0300

    Trying to optimize routes

--- src/components/payment/paymentLayout.jsx
+++ src/components/payment/paymentLayout.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 const PaymentLayout = ({ children }) => {
   const navigate = useNavigate();
   const totalSum = useSelector((state) => state.basket.sumPrice);
-
   console.log('totalSum from paymentLayout: ', totalSum);
 
   const redirectHandler = () => {
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -3,45 +3,31 @@ import React from 'react';
 import MainLayout from '../components/layout/mainLayout';
 import GoodsSelection from '../components/main/goodsSelection';
 import Cocktail from '../components/main/goodsSelection/Cocktail/Cocktail';
-import AuthLayout from '../components/auth/authLayout';
 import Registration from '../components/auth/registration';
 import Login from '../components/auth/login';
 import Basket from '../components/header/basket/basket';
-import PaymentLayout from '../components/payment/paymentLayout';
 import Payment from '../components/payment';
 import PageNotFound from '../components/pageNotFound';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const navigate = useNavigate();
+const totalSum = useSelector((state) => state.basket.sumPrice);
 
 export const publicRoutes = [
   { path: '/', element: <AppLayout /> },
   { path: '/', element: <MainLayout /> },
   { path: ':ingredientName', element: <GoodsSelection /> },
   { path: 'cocktail/:id', element: <Cocktail /> },
-  {
-    path: 'registration',
-    element: (
-      <AuthLayout>
-        <Registration />
-      </AuthLayout>
-    ),
-  },
-  {
-    path: 'login',
-    element: (
-      <AuthLayout>
-        <Login />
-      </AuthLayout>
-    ),
-  },
   { path: 'basket', element: <Basket /> },
   {
     path: 'payment',
-    element: (
-      <PaymentLayout>
-        <Payment />
-      </PaymentLayout>
-    ),
+    element: totalSum ? <Payment /> : navigate('/Light%20rum', { replace: true }),
   },
   { path: '*', element: <PageNotFound /> },
+  { path: 'registration', element: <Registration /> },
+  { path: 'login', element: <Login /> },
+  { path: '*', element: navigate('/Light%20rum', { replace: true }) },
 ];
 
 export const privateRouter = [];
